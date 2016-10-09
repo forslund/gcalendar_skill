5bfae4cfdd2ef8b44083c34362c23523c989fadb
Åke Forslund <ake.forslund@gmail.com>
2016-10-09 10:39:56 +0200

    Fix next appointment for the day after tomorrow and later

--- __init__.py
+++ __init__.py
@@ -129,6 +129,7 @@ class GoogleCalendarSkill(MycroftSkill):
             start = event['start'].get('dateTime', event['start'].get('date'))
             d = dt.datetime.strptime(start.split('+')[0], '%Y-%m-%dT%H:%M:%S')
             starttime = d.strftime('%H . %M')
+            startdate = d.strftime('%-m %B')
             if d.date() == dt.datetime.today().date():
                 data = {'appointment': event['summary'],
                         'time': starttime}
